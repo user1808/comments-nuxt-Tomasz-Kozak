3c1f63097268ab7ad9a376132b9490c52f4441b0
user1808 <tomasz.kozak04@gmail.com>
2022-10-18 11:20:16 +0200

    add css classes to table

--- components/Content/CommentsTable.vue
+++ components/Content/CommentsTable.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="comments-table">
         <div class="comments-title">
-            <span>Komentarze wspierających</span>
-            <div>Liczba</div>
+            <div class="comments-title-text">
+                Komentarze wspierających
+            </div>
+            <div class="comments-title-number">
+                {{ comments.length }}
+            </div>
         </div>
-        <div class="comments-content">
+        <div class="comments-content" v-if="comments.length > 0">
             <CommentCard v-for="comment in comments" :key="comment.id" :comment="comment" />
+        </div>
+        <div class="no-comments-content" v-else>
+            Bądź pierwszym komentującym!
         </div>
     </div>
 </template>
@@ -34,5 +41,32 @@ const comments: Array<Comment> = [{
 <style lang="scss" scoped>
 .comments-content {
     padding: 2rem 0;
+}
+.comments-title {
+    display: flex;
+}
+.comments-title-text {
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+}
+.comments-title-number {
+    border-radius: 50%;
+    width: 23px;
+    height: 23px;
+
+    border: 2px solid #1669c4;
+    color: #1669c4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin: 0 0.5rem;
+}
+.no-comments-content {
+    padding: 2rem 0;
+    text-align: center;
+    font-size: 24px;
+    color: grey;
 }
 </style>
